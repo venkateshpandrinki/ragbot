8ae04ed181d7b24807df8bbace2721ad32ddbdf0
venkateshpandirnki <venkateshpandrinki3@gmail.com>
2025-08-13 10:11:57 +0530

    fixed embed bugs

---
+++ app/api/chat/route.ts
@@ -0,0 +1,41 @@
+import { createResource } from '@/lib/actions/resources';
+import { convertToCoreMessages, streamText, tool } from 'ai';
+import { google } from '@ai-sdk/google';
+import { z } from 'zod';
+import { findRelevantContent } from '@/lib/ai/embedding';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = await streamText({
+     model: google('gemini-2.0-flash'),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
+    messages: convertToCoreMessages(messages),
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+     getInformation: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        parameters: z.object({
+          question: z.string().describe('the users question'),
+        }),
+        execute: async ({ question }) => findRelevantContent(question),
+      })
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
---
+++ lib/ai/embedding.ts
@@ -0,0 +1,72 @@
+import {embedMany,embed} from 'ai';
+import {google} from '@ai-sdk/google'
+import {db} from '../db';
+import { cosineDistance,desc,gt,sql } from 'drizzle-orm';
+import { embeddings } from '../db/schema/embeddings';
+// const embeddingModel = google.textEmbeddingModel('gemini-embedding-exp-03-07'); 
+const model = google.textEmbeddingModel('text-embedding-004')
+
+
+
+const generateChunks = (input: string): string[] => {
+    return input
+      .trim()
+      .split('.')
+      .filter(i => i !== '');
+  };
+
+
+  export const generateEmbeddings = async (
+    value: string,
+  ): Promise<Array<{ embedding: number[]; content: string }>> => {
+    const chunks = generateChunks(value);
+    const { embeddings } = await embedMany({
+      model,
+      values: chunks,
+       // @ts-ignore - Ignore TypeScript errors for the next line
+    // providerOptions: {
+    //   google: {
+    //    outputDimensionality: 1536, // Specify desired dimension if needed/possible
+    //     taskType: 'RETRIEVAL_QUERY'
+    //   },
+    // }
+    });
+    console.log(embeddings[0].length)
+
+    return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
+  };
+  
+
+  
+  export const generateEmbedding = async (value:string): Promise<number[]> =>{
+    const input = value.replaceAll('\\n',' ');
+    const {embedding}=  await embed ({
+      model,
+      value:input,
+      //@ts-ignore
+  //        providerOptions: {
+  //   google: {
+  //     outputDimensionality: 1536, // Specify desired dimension if needed/possible
+  //       taskType: 'RETRIEVAL_QUERY'
+  //   },
+  // },
+    });
+    console.log(embedding.length)
+    return embedding;
+  };
+
+  export const findRelevantContent = async (userQuery:string) => {
+    const userQueryEmbedded = await generateEmbedding(userQuery);
+     const similarity = sql<number>`1 - (${cosineDistance(
+      embeddings.embedding, userQueryEmbedded,
+  )})`;
+  const similarGuides = await db
+    .select({ name: embeddings.content, similarity })
+    .from(embeddings)
+    .where(gt(similarity, 0.5))
+    .orderBy(t => desc(t.similarity))
+    .limit(4);
+     console.log("DEBUG - findRelevantContent result:", JSON.stringify(similarGuides, null, 2));
+return { results: similarGuides }; // wraps array in an object
+
+  }
